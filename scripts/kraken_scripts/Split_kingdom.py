f3035cb04f66db1dc4a6e4a3dc2c046df7186c29
Pedro J. Torres <pjtorres88@gmail.com>
2017-07-05 14:21:36 -0700

    Update Split_kingdom.py

--- scripts/kraken_scripts/Split_kingdom.py
+++ scripts/kraken_scripts/Split_kingdom.py
@@ -4,17 +4,17 @@ import os
 import sys,getopt
 import argparse
 
-"This script will split kraken-mpa output based on kingdom in the merged kraken files aka split original file based on wether the abundance
-line belings to Bacteria, Virus, Archea, or Eukaryota (Protist and fungi)"
+"This script will split kraken-mpa output based on kingdom in the merged kraken files aka split original file based on wether the abundance line belings to Bacteria, Virus, Archea, or Eukaryota (Protist and fungi)"
+
+#--------command line arguments----------------------
 parser=argparse.ArgumentParser(description="Script will split kraken-mpa output based on Bacteria, Virus, Fungi, and Protist")
 parser.add_argument('-i','--input', help=' Input file name aka the output from running kraken-mpa',required=True)
 parser.add_argument('-o','--output',help='Output directory name.', required=False)
 args = parser.parse_args()
-
-inputfile=str(args.input)
+inputfile=str(args.input) #name of input file '-i'
                                
 #--------Create new directory for file output-----
-path=str(args.output) #'Split_kraken_domain'
+path=str(args.output) #name of new output directory
 if not os.path.exists(path):
     os.makedirs(path)
 
@@ -23,11 +23,12 @@ Bacteria=os.path.join(path,"Bacteria"+".txt")
 Viruses=os.path.join(path,"Viruses"+".txt")
 Archaea=os.path.join(path,"Archaea"+".txt")
 Eukaryota=os.path.join(path,"Eukaryota"+".txt")
-#-------------open file output from kraken                                                           
+
+#-------------open file output from kraken---------------------                                                           
 fin=open(inputfile, "r+")
-header=fin.readline() # header containing sample ids
-                               
-#----Open all new files which will contain counts for specific domain
+header=fin.readline() # header containing sample ids         
+
+#----Open all new files which will contain counts for specific domain-------
 fout_b=open(Bacteria,"w")
 fout_b.write(header)
 fout_v=open(Viruses,"w")
@@ -36,11 +37,11 @@ fout_a=open(Archaea,"w")
 fout_a.write(header)
 fout_e=open(Eukaryota,"w")
 fout_e.write(header)  
+
 #--------read through the file and sperate og file into 4 new files-------                              
 for line in fin:
     sep_line=line.split('\t')# seperate based on tab in txt file
     domain=sep_line[0].split('|')# only look at the domain
-
     if domain[0]=="d__Bacteria":
         fout_b.write(line)
     elif domain[0]=="d__Viruses":
