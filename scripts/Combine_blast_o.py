ae56e7c0e344c2a419584c834e61ecfa21823ddf
Pedro J. Torres <pjtorres88@gmail.com>
2017-09-13 15:32:13 -0700

    Update Combine_blast_o.py

--- scripts/Combine_blast_o.py
+++ scripts/Combine_blast_o.py
@@ -2,21 +2,17 @@
 import argparse
 import os
 
-######################################################################################################
-######### Split to count the number of hits given by a blast output and make an OTU style format
-######################################################################################################
+#---------- Split to count the number of hits given by a blast output and make an OTU style format------
 
-#####################################################################################################
-####### Make dictionary and call each txt file in directory
-####################################################################################################
+
+#-----------------Make dictionary and call each txt file in directory-------------------------------
 """make dictionary to keep count of blast outputs. Keys are going to be taxa name and a list will keep track of the abundance for each file"""
 taxa={}
 # All txt files (should be blast output txt files) in current directory are added to a list 'F'
 F=[i for i in os.listdir(os.curdir) if i.split(".")[-1]=="txt"]
 print ("You are combining " +str(len(F))+" text Files")
-#####################################################################################################
-####### Call and open each txt file in directory and start reading each line which should be taxonomy/gene name
-####################################################################################################
+
+#------------Call and open each txt file in directory and start reading each line which should be taxonomy/gene name---------
 for myfile in F:
     f=open(myfile,"r")
     for line in f:
@@ -32,9 +28,8 @@ for myfile in F:
 print "There are a total of " +str(len(taxa)) +" taxa"
 """Above it does two things. First it will be calling the values to the Key 'taxName'. But because there are a list of counters (one for each file) we need to tell it which one. By indexing all the files in 'myfile' you keep everything in order, call the right list index and add 1 each time you see that taxaName in that particular file."""
 
-#####################################################################################################
-#######Create new output file and write in abundance
-####################################################################################################
+#-----------------Create new output file and write in abundance
+
 o=open("Blast_abundance.txt","w+")
 o.write("Taxa\t"+"\t".join(F)+"\n")
 for i in taxa:
